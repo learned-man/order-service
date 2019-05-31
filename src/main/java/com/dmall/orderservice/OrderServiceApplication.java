eba17cb3f6e34ad0a1a0d7983ac4419b85d9000c
Knight Wu <knight.wu@oocl.com>
2019-05-31 16:46:47 +0800

    hystrix

--- src/main/java/com/dmall/orderservice/OrderServiceApplication.java
+++ src/main/java/com/dmall/orderservice/OrderServiceApplication.java
@@ -3,6 +3,7 @@ package com.dmall.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,7 +11,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @EnableFeignClients
+@EnableHystrixDashboard
+@EnableHystrix
 //TODO: enable hystrix and dashboard
+
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
